6151ea29d2b7025e538a31e918cb3d2f777f060d
Federicogonzalez99 <fedegonzalez-99@hotmail.com>
2019-03-26 11:07:21 -0300

    se agrego clase 4

---
+++ clase4/Ingresos.h
@@ -0,0 +1,10 @@
+
+
+
+/** \brief le pide numeros al usuario el ingreso de un numero
+ *
+ * \param [] char el mensaje a ser mostrado
+ * \return int el numero puesto por el usuario
+ *
+ */
+int pedirEntero(char []);
---
+++ clase4/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Ingresos.h"
+#include "recursividad.h"
+int main()
+{
+    /*
+    int edad;
+    int legajo;
+    int peso;
+    edad = pedirEntero("Ingrese edad:");
+    legajo = pedirEntero("Ingrese su legajo:");
+    peso = pedirEntero("Ingrese su peso:");
+    */
+    int resultado = factorial(5);
+    printf("%d", resultado);
+    return 0;
+}
